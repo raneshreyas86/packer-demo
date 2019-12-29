c56e19e70b7fe120b5c3be144a1f8d4df8018cd3
raneshreyas86 <raneshreyas86@gmail.com>
2019-12-29 10:57:47 -0600

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-sudo yum update
-sudo yum install -y nginx
-sudo yum install nodejs npm --enablerepo=epel
+apt-get update
+apt-get install -y nginx nodejs npm
 
-sudo groupadd node-demo
-sudo useradd -d /app -s /bin/false -g node-demo node-demo
+groupadd node-demo
+useradd -d /app -s /bin/false -g node-demo node-demo
 
-mv /app /app
-sudo chown -R node-demo:node-demo /app
+mv /tmp/app /app
+chown -R node-demo:node-demo /app
 
 echo 'user root;
 worker_processes auto;
 pid /run/nginx.pid;
-
 events {
         worker_connections 768;
         # multi_accept on;
 }
-
 http {
   server {
     listen 80;
@@ -29,5 +26,22 @@ http {
   }
 }' > /etc/nginx/nginx.conf
 
-sudo service nginx start
+service nginx restart
 
+cd /app
+npm install
+
+echo '[Service]
+ExecStart=/usr/bin/nodejs /app/index.js
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=node-demo
+User=node-demo
+Group=node-demo
+Environment=NODE_ENV=production
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/node-demo.service
+
+systemctl enable node-demo
+systemctl start node-demo
